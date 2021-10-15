7049cabf2ecda6bcead25bf8e67f96df5eb2ce69
rddmoon <yaniar0416@gmail.com>
2021-10-15 16:48:41 +0700

    hmm

--- effect.js
+++ effect.js
@@ -7,11 +7,11 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5').stop();
-			$('#b11').animate({top:240, left: vw-250},500);
-			$('#b22').animate({top:240, left: vw-150},500);
-			$('#b33').animate({top:240, left: vw-50},500);
-			$('#b44').animate({top:240, left: vw+50},500);
-			$('#b55').animate({top:240, left: vw+150},500);
+			$('#b11').animate({top:240, left: vw-200},500);
+			$('#b22').animate({top:240, left: vw-100},500);
+			$('#b33').animate({top:240, left: vw-0},500);
+			$('#b44').animate({top:240, left: vw+100},500);
+			$('#b55').animate({top:240, left: vw+200},500);
 		});
 
 	$('#turn_on').click(function(){
